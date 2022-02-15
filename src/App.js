3ca3cda8116e3960da558870039fb05497250202
Akshata290494 <92198619+Akshata290494@users.noreply.github.com>
2022-02-15 17:10:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import PersonList from './components/PersonList.js';
+import PersonAdd from './components/PersonAdd';
+
+function App() {
+  return (
+    <div ClassName="App">
+      <PersonList/>
+      <PersonAdd/>
+    </div>
+  )
+}
+export default App;
